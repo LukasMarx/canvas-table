6a43ed2bde3afbeac41c5225823ebc3b30cd64b7
Lukas <marxlukas@googlemail.com>
2021-12-04 21:51:43 +0100

    feat(dev): added storybook

---
+++ src/stories/Table.stories.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import example from "../generated.json";
+
+import { Table } from "../lib/Table";
+import { ColumnConfig } from "../lib/types/ColumnConfig";
+
+const columnConfig = [
+  {
+    field: "name",
+    width: 150,
+  },
+  {
+    field: "index",
+    width: 80,
+  },
+  {
+    field: "guid",
+    width: 300,
+  },
+
+  {
+    field: "isActive",
+    width: 100,
+  },
+  {
+    field: "balance",
+    width: 100,
+  },
+  {
+    field: "gender",
+    width: 100,
+  },
+  {
+    field: "age",
+    width: 100,
+    formatterParams: {
+      alignHorizontal: "left",
+    },
+  },
+  {
+    field: "eyeColor",
+    width: 100,
+  },
+  {
+    field: "company",
+    width: 100,
+  },
+  {
+    field: "email",
+    width: 250,
+  },
+  {
+    field: "phone",
+    width: 150,
+  },
+  {
+    field: "address",
+    width: 400,
+  },
+  {
+    field: "latitude",
+    width: 100,
+  },
+  {
+    field: "longitude",
+    width: 100,
+  },
+] as ColumnConfig[];
+
+export default {
+  title: "Table",
+  component: Table,
+} as ComponentMeta<typeof Table>;
+
+const Template: ComponentStory<typeof Table> = (args) => (
+  <div style={{ height: 500 }}>
+    <Table {...args} />
+  </div>
+);
+
+export const Basic = Template.bind({});
+Basic.args = {
+  data: example as any[],
+  columns: columnConfig,
+};
